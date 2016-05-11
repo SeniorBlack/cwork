c9e749ced88b81993b30f6dea8c6d0d8124c91b9
feelm <845553422@qq.com>
2016-05-11 15:51:46 +0800

    add mystrcpy()

--- string_operation/mystring.c
+++ string_operation/mystring.c
@@ -3,7 +3,7 @@
 */
 
 #include <stdio.h>
-	
+
 size_t mystrlen(const char *s) {
 	size_t len = 0;
 	for(; *s != '\0'; s++) {
@@ -12,7 +12,24 @@ size_t mystrlen(const char *s) {
 	return len;
 }
 
+char * mystrcpy(char *dest, const char *src) {
+	while(1) {
+		*dest = *src;
+		if() {
+			*src = '\0';
+			break;
+		}
+		dest++;
+		src++;
+	}
+	return dest;
+}
 int main() {
+	//mystrlen():
 	printf("mystrlen(new rock cloud) = %d\n", mystrlen("new rock cloud"));
+	//mystrcpy():
+	char dest[20] = {'n', 'e', 'w', '\0'};
+	char *src = "hello, newrock!";
+	printf("dest[20] = %s, src = %s, mystrcpy(dest, src) = %s\n", dest, src, mystrcpy(dest, src));
 	return 0;
 }
