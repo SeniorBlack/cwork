5e64ac6c96a7b4c71887d2c0b9254fe11f3fb645
sma <845553422@qq.com>
2016-05-17 15:35:30 +0800

    可以编译运行，输出到output.log

--- apt/apt.c
+++ apt/apt.c
@@ -12,30 +12,38 @@ typedef struct node {
 	struct node *p_next;
 } data_node;
 
+data_node *head = NULL;
 //在末尾添加时间戳分割的节点
-void tail_add(data_node *head, char *p) {
+void tail_add(char *p) {
 	data_node *tmp = (data_node *)malloc(sizeof(data_node));
 	strcpy(tmp->stamp, p);
 	tmp->p_next = NULL;
-	data_node *p_node =  NULL;
-	for(p_node = head;p_node;p_node = p_node->p_next) {
-		if(!p_node->p_next) {
-			p_node->p_next = tmp;
-			break;
+	if(head == NULL) {
+		head = tmp;
+	} else { 
+		data_node *p_node =  NULL;
+		for(p_node = head;p_node;p_node = p_node->p_next) {
+			if(!p_node->p_next) {
+				p_node->p_next = tmp;
+				break;
+			}
 		}
 	}
 	tmp = NULL;
-
 }
 
+//根据被叫号码找Call-ID
+char *
+
 //输出所有有节点到文件
-void node_output(data_node *head) {
+void node_output() {
 	FILE *p_outfile = fopen("output.log", "w");
 	if(p_outfile == NULL) {
 		printf("failed to open outfile");
 		exit(1);
 	}
 	data_node *p_node =  NULL;
+	fwrite(head->stamp, strlen(head->stamp), 1, p_outfile);
 	for (p_node = head;p_node;p_node = p_node->p_next) {
         if (p_node->p_next) {
             fwrite(p_node->p_next->stamp, strlen(p_node->p_next->stamp), 1, p_outfile);
@@ -49,7 +57,7 @@ void node_output(data_node *head) {
 int main() {
 	char ch;
 	char str_buf[1024] = {'\0'};
-	data_node *head = NULL;
+	char buffer[128] = {'\0'};
 	//打开文件
 	FILE *p_file = fopen("message.log", "r");
 	if(p_file == NULL) {
@@ -57,19 +65,17 @@ int main() {
 		exit(1);
 	}
 	//读取一个时间戳到str_buf
-	while((ch=fgetc(p_file)) != EOF) {
-		if(ch == "[") {
-			if(str_buf[0] != '\0') {
-				tail_add(head, str_buf);
-			}
-			memset(str_buf, '\0', 1024)
-			continue;
+	while(fgets(buffer, 128, p_file) != NULL) {
+		strcat(str_buf, buffer);
+		printf("buffer = %s, str_buf = %s\n", buffer, str_buf);
+		if(strchr(str_buf, '[')) {
+			tail_add(str_buf);
+			memset(str_buf, '\0', 1024);
 		}
-		int i = 0;
-		str_buf[i++] = ch;
+		buffer[0] = '\0';
 	}
-	//输出所有节点
-	node_outpt(head);       
+	//输出所有节点到文件
+	node_output(head);       
 	fclose(p_file);
 	return 0;
 }
